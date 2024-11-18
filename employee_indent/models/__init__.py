777474c018f579f54e69ffec71f3a40a729efeba
RAMKI-NITS <ramakrishnan.m@navabrindit.com>
2024-11-18 18:25:30 +0530

    Employee Indent and Multi Approval Level

---
+++ employee_indent/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import models, job_levels, position_name_master
---
+++ multi_level_approval/models/multi_approval_type_line.py
@@ -0,0 +1,36 @@
+##############################################################################
+#
+#    Copyright Domiup (<http://domiup.com>).
+#
+##############################################################################
+
+import logging
+
+from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class MultiApprovalTypeLine(models.Model):
+    _name = "multi.approval.type.line"
+    _description = "Multi Aproval Type Lines"
+    _order = "sequence"
+
+    name = fields.Char(string="Title", required=True)
+    user_id = fields.Many2many(string="User", comodel_name="res.users", required=True)
+    sequence = fields.Integer()
+    require_opt = fields.Selection(
+        [
+            ("Required", "All Must Approve"),
+            ("Optional", "Any One Can Approve"),
+        ],
+        string="Type of Approval",
+        default="Required",
+        required=True,
+    )
+    type_id = fields.Many2one(string="Type", comodel_name="multi.approval.type")
+
+    def get_user(self):
+        self.ensure_one()
+        print(self.user_id.ids)
+        return self.user_id.ids
