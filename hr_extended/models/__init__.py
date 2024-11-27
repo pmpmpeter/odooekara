92978468b3cd2fbdde143e98631ac1858ebfc8bc
surya-nits <surya.n@navabrindit.com>
2024-11-27 16:49:33 +0530

    Revert "Staging"

--- hr_extended/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import hr_config
-from . import recruitement_invitation
-from . import pre_employment_check
-from . import employee_indent
-from . import hr_applicant
-from . import project_project
