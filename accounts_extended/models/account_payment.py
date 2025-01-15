cadbcd04e2a74dfdc2ec73f3265ff9a4bd348e6f
surya-nits <surya.n@navabrindit.com>
2025-01-15 13:35:18 +0530

    Accounting points

--- accounts_extended/models/account_payment.py
+++ accounts_extended/models/account_payment.py
@@ -52,11 +52,11 @@ class AccountPayment(models.Model):
 
     def action_post(self):
         for pay in self:
-            if pay.state != 'approved':
+            if pay.state != 'approved' and pay.payment_type == 'outbound':
                 raise ValidationError('You cannot confirm payments that are not Approved.')
             if pay.payment_method_line_id.name == 'Cheque' and not pay.is_cheque_cleared and pay.payment_type == 'outbound':
-                raise UserError(_("Alert !! Kindly Clear the cheque and Update the utr number."))
-            if not pay.utr_number and pay.payment_type == 'outbound':
+                raise UserError(_("Alert !! Kindly Clear the cheque"))
+            if not pay.utr_number and (pay.payment_type == 'outbound' or pay.is_fund_requsiting):
                 raise UserError(_("Alert !! Kindly update the UTR Number."))
             if pay.amount <=0:
                 raise UserError(_("Alert !! Amount should be greated than Zero"))
