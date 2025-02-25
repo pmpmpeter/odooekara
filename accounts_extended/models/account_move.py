b279ab455f0770e7ab94e873f836b156f6dfe82b
dell <afnasamad.pk@navabrindit.com>
2025-02-25 13:32:43 +0530

    UAT update

--- accounts_extended/models/account_move.py
+++ accounts_extended/models/account_move.py
@@ -64,6 +64,7 @@ EMPTY = object()
 
 BILL_APPR = ['in_invoice', 'in_receipt', 'in_refund']
 
+
 class AccountMoveInherit(models.Model):
     _inherit = 'account.move'
 
@@ -84,8 +85,8 @@ class AccountMoveInherit(models.Model):
     )
     expense_sequence = fields.Char(string='Task ID')
     expense_type = fields.Selection([
-    ("capex", "Capex"),
-    ("opex", "Opex")], default='opex', string="Capex/Opex")
+        ("capex", "Capex"),
+        ("opex", "Opex")], default='opex', string="Capex/Opex")
     expense_invoice_no = fields.Char(string="Tax/Proforma Invoice No")
     expense_invoice_type_id = fields.Many2one('invoice.type', string="Type of Invoice")
     expense_user_id = fields.Many2one('res.users', string="Requested By", default=lambda self: self.env.user)
@@ -117,9 +118,10 @@ class AccountMoveInherit(models.Model):
                 ('type', 'in', journal_type),
             ])
 
-    @api.depends('company_id', 'partner_id', 'amount_total', 'currency_id', 'invoice_line_ids.quantity', 'invoice_line_ids.price_unit','amount_untaxed_signed')
+    @api.depends('company_id', 'partner_id', 'amount_total', 'currency_id', 'invoice_line_ids.quantity',
+                 'invoice_line_ids.price_unit', 'amount_untaxed_signed')
     def _compute_partner_tcs_warning(self):
-        msg=''
+        msg = ''
         for move in self.filtered(lambda move: move.partner_id.commercial_partner_id.tcs_applicable):
             move.with_company(move.company_id)
             move.partner_tcs_warning = ''
@@ -127,19 +129,20 @@ class AccountMoveInherit(models.Model):
             domain1 = [('date_from', '<=', invoice_date), ('date_to', '>=', invoice_date)]
             fiscal_year = self.env['account.fiscal.year'].sudo().search(domain1, limit=1)
             fiscal_year_start_date = fiscal_year_end_date = invoice_date
-            basic_amount=0
+            basic_amount = 0
             if fiscal_year:
                 fiscal_year_start_date = fiscal_year.date_from
                 fiscal_year_end_date = fiscal_year.date_to
-            if move.partner_id and move.amount_untaxed_signed>0:
+            if move.partner_id and move.amount_untaxed_signed > 0:
                 query1 = """
                         SELECT sum(am.amount_untaxed_signed) as amount_untaxed_signed
                         FROM account_move am
                         where am.move_type in ('out_invoice', 'out_refund', 'out_receipt') and am.partner_id=%s
                         and am.date<=%s and am.date>=%s and am.state='posted';
                         """
-                query_params1 = (move.partner_id.commercial_partner_id.id, str(fiscal_year_end_date), str(fiscal_year_start_date))
-                data_get1= self.env.cr.execute(query1, query_params1)
+                query_params1 = (
+                move.partner_id.commercial_partner_id.id, str(fiscal_year_end_date), str(fiscal_year_start_date))
+                data_get1 = self.env.cr.execute(query1, query_params1)
                 lines1 = self.env.cr.dictfetchall()
                 if lines1[0].get('amount_untaxed_signed') and lines1[0].get('amount_untaxed_signed') != None:
                     basic_amount = lines1[0].get('amount_untaxed_signed')
@@ -152,8 +155,10 @@ class AccountMoveInherit(models.Model):
             tcs_limit_amount = self.partner_id.commercial_partner_id.tcs_limit_amount_partner or self.company_id.tcs_limit_amount
             if show_warning and tcs_limit and basic_amount > float(tcs_limit_amount):
                 basic_amount_formatted = formatLang(self.env, basic_amount, currency_obj=move.company_id.currency_id)
-                tcs_limit_amount_formatted = formatLang(self.env, float(tcs_limit_amount), currency_obj=move.company_id.currency_id)
-                msg = "Cummulative Sales for - %s in %s is %s which is exceeding TCS limit of %s."%(move.partner_id.name, fiscal_year.display_name, basic_amount_formatted, tcs_limit_amount_formatted)
+                tcs_limit_amount_formatted = formatLang(self.env, float(tcs_limit_amount),
+                                                        currency_obj=move.company_id.currency_id)
+                msg = "Cummulative Sales for - %s in %s is %s which is exceeding TCS limit of %s." % (
+                move.partner_id.name, fiscal_year.display_name, basic_amount_formatted, tcs_limit_amount_formatted)
         self.partner_tcs_warning = msg
 
     @api.depends('company_id', 'partner_id', 'amount_total', 'currency_id', 'invoice_line_ids.quantity',
@@ -161,7 +166,6 @@ class AccountMoveInherit(models.Model):
     def _compute_partner_tds_warning(self):
         msg = ''
         for move in self.filtered(lambda move: move.partner_id.commercial_partner_id.tds_applicable):
-            print("Case11111111111111111")
             move.with_company(move.company_id)
             move.partner_tds_warning = ''
             invoice_date = move.invoice_date or move.date or fields.Date.today()
@@ -180,7 +184,7 @@ class AccountMoveInherit(models.Model):
                            and am.date<=%s and am.date>=%s and am.state='posted';
                            """
                 query_params1 = (
-                move.partner_id.commercial_partner_id.id, str(fiscal_year_end_date), str(fiscal_year_start_date))
+                    move.partner_id.commercial_partner_id.id, str(fiscal_year_end_date), str(fiscal_year_start_date))
                 data_get1 = self.env.cr.execute(query1, query_params1)
                 lines1 = self.env.cr.dictfetchall()
                 if lines1[0].get('amount_untaxed_signed') and lines1[0].get('amount_untaxed_signed') != None:
@@ -198,10 +202,11 @@ class AccountMoveInherit(models.Model):
                 tds_limit_amount_formatted = formatLang(self.env, float(tds_limit_amount),
                                                         currency_obj=move.company_id.currency_id)
                 msg = "Cummulative Purchases for - %s in %s is %s which is exceeding TDS limit of %s. Kindly deduct %s." % (
-                move.partner_id.name, fiscal_year.display_name, basic_amount_formatted, tds_limit_amount_formatted, tds_tax_id.display_name)
+                    move.partner_id.name, fiscal_year.display_name, basic_amount_formatted, tds_limit_amount_formatted,
+                    tds_tax_id.display_name)
         self.partner_tds_warning = msg
 
-    @api.depends('approval_document.type_id.state','approval_document.line_ids.state')
+    @api.depends('approval_document.type_id.state', 'approval_document.line_ids.state')
     def compute_approval_state(self):
         for record in self:
             if record.approval_document:
@@ -209,7 +214,8 @@ class AccountMoveInherit(models.Model):
                 if all(state == 'Draft' for state in line_states):
                     record.approval_state = 'Waiting For Approval'
                 elif 'Waiting for Approval' in line_states:
-                    waiting_lines = record.approval_document.line_ids.filtered(lambda l: l.state == 'Waiting for Approval')
+                    waiting_lines = record.approval_document.line_ids.filtered(
+                        lambda l: l.state == 'Waiting for Approval')
                     if waiting_lines:
                         record.approval_state = f"Waiting for {', '.join(waiting_lines.mapped('name'))} Approval"
                 elif all(state == 'Approved' for state in line_states):
@@ -219,8 +225,11 @@ class AccountMoveInherit(models.Model):
                 elif 'Cancel' in line_states:
                     record.approval_state = 'Cancelled'
             else:
-                entry_rec = self.env['multi.approval.type'].sudo().search([('model_id','=','account.move'),('state','=','confirm'),('description','=','Journal Entries')],limit=1)
-                rec = self.env['multi.approval.type'].sudo().search([('model_id','=','account.move'),('state','=','confirm')],limit=1)
+                entry_rec = self.env['multi.approval.type'].sudo().search(
+                    [('model_id', '=', 'account.move'), ('state', '=', 'confirm'),
+                     ('description', '=', 'Journal Entries')], limit=1)
+                rec = self.env['multi.approval.type'].sudo().search(
+                    [('model_id', '=', 'account.move'), ('state', '=', 'confirm')], limit=1)
                 if record.move_type == 'entry':
                     if entry_rec:
                         record.approval_state = 'To Submit for Approval'
@@ -231,6 +240,18 @@ class AccountMoveInherit(models.Model):
                         record.approval_state = 'To Submit for Approval'
                     else:
                         record.approval_state = 'Not Applicable'
+                elif record.move_type == 'in_invoice':
+                    if rec:
+                        record.approval_state = 'To Submit for Approval'
+
+    def button_draft(self):
+        super().button_draft()
+        for record in self:
+            rec = self.env['multi.approval.type'].sudo().search(
+                [('model_id', '=', 'account.move'), ('state', '=', 'confirm')], limit=1)
+            if rec:
+                record.approval_state = 'To Submit for Approval'
+                record.x_has_request_approval = False
 
     def button_cancel(self):
         # Shortcut to move from posted to cancelled directly. Useful for E-invoices that must not be changed
@@ -244,7 +265,17 @@ class AccountMoveInherit(models.Model):
             raise UserError(_("Only draft or to approved journal entries can be cancelled."))
 
         # Write state change to 'cancel'
-        self.write({'auto_post': 'no', 'state': 'cancel'})
+        model_name = 'account.move'
+        res_id = self.id
+        origin_ref = f"{model_name},{res_id}"
+        existing_approvals = self.env['multi.approval'].search([("origin_ref", "=", origin_ref)])
+        existing_approvals.write({'state': 'Cancel'})
+        self.write({
+            'auto_post': 'no',
+            'approval_state': 'Not Applicable',
+            'x_has_request_approval': False,
+            'state': 'cancel'
+        })
 
     def action_post(self):
         for rec in self:
@@ -255,13 +286,13 @@ class AccountMoveInherit(models.Model):
         for rec in self:
             if rec.move_type == 'in_invoice' and rec.partner_id.tds_applicable:
                 wiz_tds = self.env['l10n_in.withhold.wizard'].with_context({
-                'active_ids': rec.ids,  # Pass the active record ID
-                'active_model': self._name  # Pass the current model name
+                    'active_ids': rec.ids,  # Pass the active record ID
+                    'active_model': self._name  # Pass the current model name
                 }).create({})
                 wiz_line_tds = self.env['l10n_in.withhold.wizard.line'].create({
-                'withhold_id': wiz_tds.id,  
-                'tax_id': rec.partner_id.tds_tax_id.id,  
-                'base': rec.amount_untaxed,  
+                    'withhold_id': wiz_tds.id,
+                    'tax_id': rec.partner_id.tds_tax_id.id,
+                    'base': rec.amount_untaxed,
                 })
                 wiz_tds.action_create_and_post_withhold()
         return res
