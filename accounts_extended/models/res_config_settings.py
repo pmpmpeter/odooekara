0c5b6499e20afd94d47154086468bfb53bb871a6
dell <afnasamad.pk@navabrindit.com>
2025-02-25 19:02:23 +0530

    UAT Update

--- accounts_extended/models/res_config_settings.py
+++ accounts_extended/models/res_config_settings.py
@@ -5,10 +5,10 @@ from datetime import datetime
 class ResCompanyInherited(models.Model):
     _inherit = 'res.company'
 
-    tax_entity1 = fields.Many2one('res.company', string="Tax Entity1")
-    share1 = fields.Integer('Share %')
-    tax_entity2 = fields.Many2one('res.company', string="Tax Entity2")
-    share2 = fields.Integer('Share %')
+    # tax_entity1 = fields.Many2one('res.company', string="Tax Entity1")
+    # share1 = fields.Integer('Share %')
+    # tax_entity2 = fields.Many2one('res.company', string="Tax Entity2")
+    # share2 = fields.Integer('Share %')
     crr_reminder_users = fields.Many2many('res.users', string="CRR & CUR Reminder Users",
                                           help="Users who will receive monthly CRR & CUR reminders")
 
