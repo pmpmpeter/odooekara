7aeac5be30fe992bba77be7a9c3bdb332150781a
fousiaAR <fousia.banu@navabrindit.com>
2025-04-07 13:41:22 +0530

    codes Updates1

--- accounts_extended/models/account_payment.py
+++ accounts_extended/models/account_payment.py
@@ -115,8 +115,8 @@ class AccountPayment(models.Model):
         for pay in self:
             if pay.state != 'approved' and pay.payment_type == 'outbound':
                 raise ValidationError('You cannot confirm payments that are not Approved.')
-            if pay.payment_method_line_id.name == 'Cheque' and not pay.is_cheque_cleared and pay.payment_type == 'outbound':
-                raise UserError(_("Alert !! Kindly Clear the cheque"))
+            # if pay.payment_method_line_id.name == 'Cheque' and not pay.is_cheque_cleared and pay.payment_type == 'outbound':
+            #     raise UserError(_("Alert !! Kindly Clear the cheque"))
             # if not pay.utr_number and (pay.payment_type == 'outbound' or pay.is_fund_requsiting):
             #     raise UserError(_("Alert !! Kindly update the UTR Number."))
             if pay.amount <= 0:
