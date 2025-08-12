742f9045cf4746425826c6ade00897cd5fce3aa5
fousiaAR <fousia.banu@navabrindit.com>
2025-08-12 17:29:46 +0530

    Codes upadted

--- partial_payment_adjustment/models/partial_payment.py
+++ partial_payment_adjustment/models/partial_payment.py
@@ -46,7 +46,7 @@ class AccountPayment(models.Model):
 
     payment_invoice_ids = fields.One2many('account.payment.invoice.line', 'payment_id', string="Customer Invoices")
     unallocated_amount = fields.Monetary(string='Residual Amount')
-
+    amount_partial_total = fields.Monetary(string='Total Amount')
 
     @api.onchange('payment_invoice_ids','amount')
     def compute_unallocated_amount(self):
@@ -240,9 +240,9 @@ class AccountPayment(models.Model):
                 total_reconcile_line_amount = 0
                 total_payment_available = total_payment_amount_main + total_reconcile_line_amount
                 # pdb.set_trace()
-                # if total_payment_available != total_reconcile_amount:
-                #     raise UserError(
-                #         _("The sum of the reconcile amount of listed invoices is not equal to payment amount."))
+                if total_payment_available != total_reconcile_amount:
+                    raise UserError(
+                        _("The sum of the reconcile amount of listed invoices is not equal to payment amount."))
                 if not payment.payment_invoice_ids.filtered(lambda line: line.reconcile_amount > 0):
                     raise UserError(
                             _("Kindly update the amount to reconcile for each transactions."))
